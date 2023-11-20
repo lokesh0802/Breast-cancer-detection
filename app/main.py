4b934db4a2b3aa17fa973e1942783754a39d65b9
Lokesh kumar <121431788+lokesh0802@users.noreply.github.com>
2023-11-21 02:54:05 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -14,57 +14,54 @@ def get_clean_data():
   
   return data
 
+def add_expander():
+    with st.expander("Breast Cell Measurements"):
+        data = get_clean_data()
 
-def add_sidebar():
-  st.sidebar.header("Breast Cell Measurements")
-  
-  data = get_clean_data()
-  
-  slider_labels = [
-        ("Radius (mean)", "radius_mean"),
-        ("Texture (mean)", "texture_mean"),
-        ("Perimeter (mean)", "perimeter_mean"),
-        ("Area (mean)", "area_mean"),
-        ("Smoothness (mean)", "smoothness_mean"),
-        ("Compactness (mean)", "compactness_mean"),
-        ("Concavity (mean)", "concavity_mean"),
-        ("Concave points (mean)", "concave points_mean"),
-        ("Symmetry (mean)", "symmetry_mean"),
-        ("Fractal dimension (mean)", "fractal_dimension_mean"),
-        ("Radius (se)", "radius_se"),
-        ("Texture (se)", "texture_se"),
-        ("Perimeter (se)", "perimeter_se"),
-        ("Area (se)", "area_se"),
-        ("Smoothness (se)", "smoothness_se"),
-        ("Compactness (se)", "compactness_se"),
-        ("Concavity (se)", "concavity_se"),
-        ("Concave points (se)", "concave points_se"),
-        ("Symmetry (se)", "symmetry_se"),
-        ("Fractal dimension (se)", "fractal_dimension_se"),
-        ("Radius (worst)", "radius_worst"),
-        ("Texture (worst)", "texture_worst"),
-        ("Perimeter (worst)", "perimeter_worst"),
-        ("Area (worst)", "area_worst"),
-        ("Smoothness (worst)", "smoothness_worst"),
-        ("Compactness (worst)", "compactness_worst"),
-        ("Concavity (worst)", "concavity_worst"),
-        ("Concave points (worst)", "concave points_worst"),
-        ("Symmetry (worst)", "symmetry_worst"),
-        ("Fractal dimension (worst)", "fractal_dimension_worst"),
-    ]
+        slider_labels = [
+            ("Radius (mean)", "radius_mean"),
+            ("Texture (mean)", "texture_mean"),
+            ("Perimeter (mean)", "perimeter_mean"),
+            ("Area (mean)", "area_mean"),
+            ("Smoothness (mean)", "smoothness_mean"),
+            ("Compactness (mean)", "compactness_mean"),
+            ("Concavity (mean)", "concavity_mean"),
+            ("Concave points (mean)", "concave points_mean"),
+            ("Symmetry (mean)", "symmetry_mean"),
+            ("Fractal dimension (mean)", "fractal_dimension_mean"),
+            ("Radius (se)", "radius_se"),
+            ("Texture (se)", "texture_se"),
+            ("Perimeter (se)", "perimeter_se"),
+            ("Area (se)", "area_se"),
+            ("Smoothness (se)", "smoothness_se"),
+            ("Compactness (se)", "compactness_se"),
+            ("Concavity (se)", "concavity_se"),
+            ("Concave points (se)", "concave points_se"),
+            ("Symmetry (se)", "symmetry_se"),
+            ("Fractal dimension (se)", "fractal_dimension_se"),
+            ("Radius (worst)", "radius_worst"),
+            ("Texture (worst)", "texture_worst"),
+            ("Perimeter (worst)", "perimeter_worst"),
+            ("Area (worst)", "area_worst"),
+            ("Smoothness (worst)", "smoothness_worst"),
+            ("Compactness (worst)", "compactness_worst"),
+            ("Concavity (worst)", "concavity_worst"),
+            ("Concave points (worst)", "concave points_worst"),
+            ("Symmetry (worst)", "symmetry_worst"),
+            ("Fractal dimension (worst)", "fractal_dimension_worst"),
+        ]
 
-  input_dict = {}
+        input_dict = {}
 
-  for label, key in slider_labels:
-    input_dict[key] = st.sidebar.slider(
-      label,
-      min_value=float(0),
-      max_value=float(data[key].max()),
-      value=float(data[key].mean()),
-      
-    )
-    
-  return input_dict
+        for label, key in slider_labels:
+            input_dict[key] = st.slider(
+                label,
+                min_value=float(0),
+                max_value=float(data[key].max()),
+                value=float(data[key].mean()),
+            )
+
+        return input_dict
 
 
 def get_scaled_values(input_dict):
