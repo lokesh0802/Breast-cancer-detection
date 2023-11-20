288db050c53a938e473422a41ff05d870acd16fa
Lokesh kumar <121431788+lokesh0802@users.noreply.github.com>
2023-11-21 01:51:43 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -270,32 +270,81 @@ def add_predictions(input_data):
 
 
 
-def main():
-  st.set_page_config(
-    page_title="Breast Cancer Predictor",
-    page_icon=":female-doctor:",
-    layout="wide",
-    initial_sidebar_state="expanded"
-  )
+# def main():
+#   st.set_page_config(
+#     page_title="Breast Cancer Predictor",
+#     page_icon=":female-doctor:",
+#     layout="wide",
+#     initial_sidebar_state="expanded"
+#   )
   
-  with open("assets/style.css") as f:
-    st.markdown("<style>{}</style>".format(f.read()), unsafe_allow_html=True)
+#   with open("assets/style.css") as f:
+#     st.markdown("<style>{}</style>".format(f.read()), unsafe_allow_html=True)
   
-  input_data = add_sidebar()
+#   input_data = add_sidebar()
   
-  with st.container():
-    st.title("Breast Cancer Predictor")
-    st.write("Please connect this app to your cytology lab to help diagnose breast cancer form your tissue sample. This app predicts using a machine learning model whether a breast mass is benign or malignant based on the measurements it receives from your cytosis lab. You can also update the measurements by hand using the sliders in the sidebar. ")
+#   with st.container():
+#     st.title("Breast Cancer Predictor")
+#     st.write("Please connect this app to your cytology lab to help diagnose breast cancer form your tissue sample. This app predicts using a machine learning model whether a breast mass is benign or malignant based on the measurements it receives from your cytosis lab. You can also update the measurements by hand using the sliders in the sidebar. ")
   
-  col1, col2 = st.columns([4,1])
+#   col1, col2,col3,col4,col5 = st.columns([4,1])
   
-  with col1:
-    radar_chart = get_radar_chart_mean(input_data)
-    st.plotly_chart(radar_chart)
-  with col2:
-    add_predictions(input_data)
+#   with col1:
+#     radar_chart_mean = get_radar_chart_mean(input_data)
+#     st.plotly_chart(radar_chart_mean)
+#     with col2:
+#         radar_chart_standard = get_radar_chart_standard(input_data)
+#         st.plotly_chart(radar_chart_standard)
+#     with col3:
+#         radar_chart_worst = get_radar_chart_worst(input_data)
+#         st.plotly_chart(radar_chart_worst)
+#     with col4:
+#         radar_chart = get_radar_chart(input_data)
+#         st.plotly_chart(radar_chart)
+#     with col5:
+#         add_predictions(input_data)
 
 
  
+# if __name__ == '__main__':
+#   main()
+def main():
+    st.set_page_config(
+        page_title="Breast Cancer Predictor",
+        page_icon=":female-doctor:",
+        layout="wide",
+        initial_sidebar_state="expanded"
+    )
+
+    with open("assets/style.css") as f:
+        st.markdown("<style>{}</style>".format(f.read()), unsafe_allow_html=True)
+
+    input_data = add_sidebar()
+
+    st.title("Breast Cancer Predictor")
+    st.write("Please connect this app to your cytology lab to help diagnose breast cancer from your tissue sample. This app predicts using a machine learning model whether a breast mass is benign or malignant based on the measurements it receives from your cytology lab. You can also update the measurements by hand using the sliders in the sidebar.")
+
+    col1, col2, col3, col4, col5 = st.columns([4, 1, 1, 4, 1])
+
+    with col1:
+        radar_chart_mean = get_radar_chart_mean(input_data)
+        st.plotly_chart(radar_chart_mean)
+
+    with col2:
+        radar_chart_standard = get_radar_chart_standard(input_data)
+        st.plotly_chart(radar_chart_standard)
+
+    with col3:
+        radar_chart_worst = get_radar_chart_worst(input_data)
+        st.plotly_chart(radar_chart_worst)
+
+    with col4:
+        radar_chart = get_radar_chart(input_data)
+        st.plotly_chart(radar_chart)
+
+    with col5:
+        add_predictions(input_data)
+
+
 if __name__ == '__main__':
-  main()
+    main()
