41b7e23f1f95737e24fe821a9bbe28aa1d71b7fb
Lokesh kumar <121431788+lokesh0802@users.noreply.github.com>
2023-11-21 02:59:43 +0530

    Update main.py

--- model/main.py
+++ model/main.py
@@ -1,57 +1,45 @@
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score, classification_report
+from sklearn.cluster import KMeans
 import pickle5 as pickle
 
 
 def create_model(data): 
-  X = data.drop(['diagnosis'], axis=1)
-  y = data['diagnosis']
+    X = data.drop(['diagnosis'], axis=1)
   
-  # scale the data
-  scaler = StandardScaler()
-  X = scaler.fit_transform(X)
-  
-  # split the data
-  X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.2, random_state=42
-  )
-  
-  # train the model
-  model = LogisticRegression()
-  model.fit(X_train, y_train)
-  
-  # test model
-  y_pred = model.predict(X_test)
-  print('Accuracy of our model: ', accuracy_score(y_test, y_pred))
-  print("Classification report: \n", classification_report(y_test, y_pred))
-  
-  return model, scaler
+    # scale the data
+    scaler = StandardScaler()
+    X_scaled = scaler.fit_transform(X)
+    
+    # perform clustering
+    kmeans = KMeans(n_clusters=2, random_state=42)
+    data['cluster'] = kmeans.fit_predict(X_scaled)
+    
+    return kmeans, scaler
 
 
 def get_clean_data():
-  data = pd.read_csv("data/data.csv")
-  
-  data = data.drop(['Unnamed: 32', 'id'], axis=1)
-  
-  data['diagnosis'] = data['diagnosis'].map({ 'M': 1, 'B': 0 })
-  
-  return data
+    data = pd.read_csv("data/data.csv")
+    
+    data = data.drop(['Unnamed: 32', 'id'], axis=1)
+    
+    data['diagnosis'] = data['diagnosis'].map({'M': 1, 'B': 0})
+    
+    return data
 
 
 def main():
-  data = get_clean_data()
+    data = get_clean_data()
 
-  model, scaler = create_model(data)
+    kmeans, scaler = create_model(data)
 
-  with open('model/model.pkl', 'wb') as f:
-    pickle.dump(model, f)
-    
-  with open('model/scaler.pkl', 'wb') as f:
-    pickle.dump(scaler, f)
+    # Save the clustering model and scaler
+    with open('model/kmeans_model.pkl', 'wb') as f:
+        pickle.dump(kmeans, f)
+        
+    with open('model/scaler.pkl', 'wb') as f:
+        pickle.dump(scaler, f)
   
 
 if __name__ == '__main__':
-  main()
+    main()
