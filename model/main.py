215a245cdd6b81f46a3c065f553d64b0a2d75c74
Lokesh kumar <121431788+lokesh0802@users.noreply.github.com>
2023-11-21 03:19:34 +0530

    Update main.py

--- model/main.py
+++ model/main.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
 from sklearn.cluster import KMeans
+from sklearn.linear_model import LogisticRegression
 import pickle5 as pickle
 
 
@@ -18,6 +19,22 @@ def create_model(data):
     return kmeans, scaler
 
 
+def train_logistic_regression_for_cluster(data, cluster, scaler):
+    cluster_data = data[data['cluster'] == cluster]
+
+    X_cluster = cluster_data.drop(['diagnosis', 'cluster'], axis=1)
+    y_cluster = cluster_data['diagnosis']
+    
+    # scale the cluster data using the original scaler
+    X_cluster_scaled = scaler.transform(X_cluster)
+
+    # train the logistic regression model
+    model = LogisticRegression()
+    model.fit(X_cluster_scaled, y_cluster)
+
+    return model
+
+
 def get_clean_data():
     data = pd.read_csv("data/data.csv")
     
@@ -31,6 +48,7 @@ def get_clean_data():
 def main():
     data = get_clean_data()
 
+    # Step 1: Clustering
     kmeans, scaler = create_model(data)
 
     # Save the clustering model and scaler
@@ -39,7 +57,17 @@ def main():
         
     with open('model/scaler.pkl', 'wb') as f:
         pickle.dump(scaler, f)
-  
+
+    # Step 2: Train Logistic Regression for each cluster
+    models = {}
+    for cluster in range(2):  # Assuming 2 clusters, adjust based on actual number
+        model = train_logistic_regression_for_cluster(data, cluster, scaler)
+        models[cluster] = model
+
+        # Save the logistic regression models
+        with open(f'model/logistic_regression_model_cluster_{cluster}.pkl', 'wb') as f:
+            pickle.dump(model, f)
+
 
 if __name__ == '__main__':
     main()
